2b6ac4ee02adfe872f789845d6b4b2c7240079e8
JamesWheadon <james.wheadon@yahoo.com>
2024-08-21 21:22:34 +0100

    test fake national grid for current intensity

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,10 +54,11 @@ tasks {
 
 dependencies {
     implementation("org.http4k:http4k-core:${http4kVersion}")
+    implementation("org.http4k:http4k-format-jackson:${http4kVersion}")
     implementation("org.jetbrains.kotlin:kotlin-stdlib:${kotlinVersion}")
+
     testImplementation("org.http4k:http4k-testing-approval:${http4kVersion}")
     testImplementation("org.http4k:http4k-testing-hamkrest:${http4kVersion}")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.2")
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.10.2")
 }
-
--- src/test/kotlin/com/learning/NationalGridContractTest.kt
+++ src/test/kotlin/com/learning/NationalGridContractTest.kt
@@ -5,9 +5,11 @@ import com.natpryce.hamkrest.equalTo
 import org.http4k.core.*
 import org.http4k.core.Method.GET
 import org.http4k.core.Status.Companion.OK
+import org.http4k.format.Jackson
 import org.http4k.routing.bind
 import org.http4k.routing.routes
 import org.junit.jupiter.api.Test
+import java.time.Instant
 
 abstract class NationalGridContractTest {
     abstract val httpClient: HttpHandler
@@ -15,6 +17,15 @@ abstract class NationalGridContractTest {
     @Test
     fun `responds when pinged on the base path`() {
         assertThat(httpClient(Request(GET, "/")).status, equalTo(OK))
+    }
+
+    @Test
+    fun `responds with forecast for the current half hour`() {
+        val currentIntensity = httpClient(Request(GET, "/intensity"))
+        val halfHourResponses = halfHourDataListLens(currentIntensity)
+
+        assertThat(currentIntensity.status, equalTo(OK))
+        assertThat(halfHourResponses.size, equalTo(1))
     }
 }
 
@@ -24,8 +35,27 @@ class FakeNationalGridTest : NationalGridContractTest() {
 
 class FakeNationalGrid : HttpHandler {
     val routes = routes(
-        "/" bind GET to { Response(OK) }
+        "/" bind GET to { Response(OK) },
+        "intensity" bind GET to {
+            val currentIntensity = Intensity(60, 60, "moderate")
+            val currentHalfHour = HalfHourData(Instant.now().toString(), Instant.now().toString(), currentIntensity)
+            Response(OK).with(halfHourDataListLens of listOf(currentHalfHour))
+        }
     )
 
     override fun invoke(request: Request): Response = routes(request)
 }
+
+data class Intensity(
+    val forecast: Long,
+    val actual: Long?,
+    val index: String
+)
+
+data class HalfHourData(
+    val from: String,
+    val to: String,
+    val intensity: Intensity
+)
+
+val halfHourDataListLens = Jackson.autoBody<List<HalfHourData>>().toLens()
