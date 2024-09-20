774dda42377b744d7abf4d70cef0b0059844966d
JamesWheadon <james.wheadon@yahoo.com>
2024-09-20 23:05:54 +0100

    Wrap Q model in a class and simplify the learning and setup

--- Scheduler/Scheduler.py
+++ Scheduler/Scheduler.py
@@ -3,39 +3,51 @@ import random
 
 from CarbonIntensityEnv import CarbonIntensityEnv
 
-alpha = 0.1
-gamma = 0.2
-epsilon = 1.0
-epsilon_min = 0.01
-epsilon_decay = 0.995
 
-num_episodes = 1000
-num_time_slots = 48
+class UseTimeScheduler:
+    def __init__(self):
+        self.alpha = 0.1
+        self.gamma = 0.2
+        self.epsilon = 1.0
+        self.epsilon_min = 0.01
+        self.epsilon_decay = 0.995
+        self.num_episodes = 1000
+        self.num_time_slots = 48
+        self.Q_table = np.zeros((self.num_time_slots, self.num_time_slots))
 
-Q_table = np.zeros((num_time_slots, num_time_slots))
-env = CarbonIntensityEnv()
+    def calculate_schedules(self):
+        intensities = [227, 239, 270, 113, 125, 190, 202, 131, 178, 207, 172, 163, 151, 109, 297, 107, 126, 159, 170,
+                       103, 143, 190, 235, 200, 244, 137, 241, 106, 265, 103, 114, 262, 156, 261, 165, 210, 183, 118,
+                       164, 241, 235, 119, 121, 160, 119, 185, 134, 263]
+        env = CarbonIntensityEnv(intensities)
+        state = 0
+        for episode in range(self.num_episodes):
+            while state != self.num_time_slots:
+                if random.uniform(0, 1) < self.epsilon:
+                    action = random.randint(state, self.num_time_slots - 1)
+                else:
+                    action = np.argmax(self.Q_table[state][state:]) + state
 
-state = env.reset()
-for episode in range(num_episodes):
-    while True:
-        if random.uniform(0, 1) < epsilon:
-            action = random.randint(state, num_time_slots - 1)
-        else:
-            action = np.argmax(Q_table[state][state:]) + state
+                reward = env.step(action)
+                best_next_action = np.argmax(self.Q_table[action])
+                self.Q_table[state, action] = (self.Q_table[state, action] + self.alpha *
+                                               (reward + self.gamma * self.Q_table[action, best_next_action] -
+                                                self.Q_table[state, action]))
+                state += 1
 
-        next_state, reward = env.step(action)
+            if self.epsilon > self.epsilon_min:
+                self.epsilon *= self.epsilon_decay
+            state = 0
 
-        best_next_action = np.argmax(Q_table[next_state])
-        Q_table[state, action] = (Q_table[state, action] + alpha *
-                                  (reward + gamma * Q_table[next_state, best_next_action] - Q_table[state, action]))
-        if state == num_time_slots - 1:
-            break
-        state = next_state
+    def get_best_action_for_time_slot(self, time_slot):
+        return np.argmax(self.Q_table[time_slot][time_slot:]) + time_slot
 
-    if epsilon > epsilon_min:
-        epsilon *= epsilon_decay
-    state = env.reset()
+    def print_q_table(self):
+        np.set_printoptions(threshold=self.num_time_slots * self.num_time_slots)
+        print(self.Q_table)
 
-np.set_printoptions(threshold=num_time_slots * num_time_slots)
-print("Final Q-table:")
-print(Q_table)
+
+if __name__ == "__main__":
+    scheduler = UseTimeScheduler()
+    scheduler.calculate_schedules()
+    print(scheduler.get_best_action_for_time_slot(20))
