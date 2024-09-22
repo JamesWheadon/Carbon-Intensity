ccc5a2ba31910f92dce75aafd077463fd3623386
JamesWheadon <james.wheadon@yahoo.com>
2024-09-22 14:39:51 +0100

    test error case when current time is not available in scheduler

--- CarbonIntensityWebApp/src/test/kotlin/com/learning/SchedulerContractTest.kt
+++ CarbonIntensityWebApp/src/test/kotlin/com/learning/SchedulerContractTest.kt
@@ -7,9 +7,12 @@ import org.http4k.core.HttpHandler
 import org.http4k.core.Method.GET
 import org.http4k.core.Request
 import org.http4k.core.Response
+import org.http4k.core.Status.Companion.NOT_FOUND
 import org.http4k.core.Status.Companion.OK
 import org.http4k.core.with
 import org.http4k.format.Jackson
+import org.http4k.lens.Query
+import org.http4k.lens.int
 import org.http4k.routing.bind
 import org.http4k.routing.routes
 import org.junit.jupiter.api.Test
@@ -23,7 +26,16 @@ interface SchedulerContractTest {
         val chargeTime = chargeTimeLens(chargeTimeResponse)
 
         assertThat(chargeTimeResponse.status, equalTo(OK))
-        assertThat(chargeTime.chargeTime, greaterThanOrEqualTo(14))
+        assertThat(chargeTime.chargeTime!!, greaterThanOrEqualTo(14))
+    }
+
+    @Test
+    fun `responds with not found error when queried with invalid time`() {
+        val chargeTimeResponse = httpClient(Request(GET, "/charge-time?current=1000"))
+        val chargeTime = chargeTimeLens(chargeTimeResponse)
+
+        assertThat(chargeTimeResponse.status, equalTo(NOT_FOUND))
+        assertThat(chargeTime, equalTo(ChargeTime(null, "no data for time slot")))
     }
 }
 
@@ -33,8 +45,13 @@ class FakeSchedulerTest : SchedulerContractTest {
 
 class FakeScheduler : HttpHandler {
     val routes = routes(
-        "/charge-time" bind GET to {
-            Response(OK).with(chargeTimeLens of ChargeTime(15))
+        "/charge-time" bind GET to { request ->
+            val current = Query.int().defaulted("current", 0)(request)
+            if (current < 96) {
+                Response(OK).with(chargeTimeLens of ChargeTime(15, null))
+            } else {
+                Response(NOT_FOUND).with(chargeTimeLens of ChargeTime(null, "no data for time slot"))
+            }
         }
     )
 
@@ -42,7 +59,8 @@ class FakeScheduler : HttpHandler {
 }
 
 data class ChargeTime(
-    val chargeTime: Int
+    val chargeTime: Int?,
+    val error: String?
 )
 
 val chargeTimeLens = Jackson.autoBody<ChargeTime>().toLens()
