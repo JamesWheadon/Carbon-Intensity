b1671909d5c5cbd31046170e05348021d7d93b23
JamesWheadon <james.wheadon@yahoo.com>
2024-10-26 14:08:16 +0100

    Add input for a start time in charge time form

--- react-app/src/ChargeTimeForm.js
+++ react-app/src/ChargeTimeForm.js
@@ -1,8 +1,15 @@
+import { useState } from 'react';
+
 function ChargeTimeForm() {
+    const [start, setStart] = useState("");
     return (
         <form>
             <label>Start time:
-                <input type="text" />
+                <input
+                    type="text"
+                    value={start}
+                    onChange={(i) => setStart(i.target.value)}
+                />
             </label>
         </form>
     );
--- react-app/src/test/ChargeTimeForm.test.js
+++ react-app/src/test/ChargeTimeForm.test.js
@@ -1,9 +1,16 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import ChargeTimeForm from '../ChargeTimeForm';
 
-test('renders learn react link', () => {
+test('renders start time label', () => {
   render(<ChargeTimeForm />);
   const startTimeLabel = screen.getByText(/Start time:/i);
   expect(startTimeLabel).toBeInTheDocument();
+});
+
+test('renders start time input field and value can update', () => {
+  render(<ChargeTimeForm />);
+  const startTimeInput = screen.getByLabelText(/Start time:/i);
+  fireEvent.change(startTimeInput, {target: {value: 'test'}});
+  expect(startTimeInput).toHaveValue("test");
 });
