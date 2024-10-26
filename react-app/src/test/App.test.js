a44dc05b979df093d6d3dc0bc09a0ae959d8e278
JamesWheadon <james.wheadon@yahoo.com>
2024-10-26 13:14:21 +0100

    add title to page

---
+++ react-app/src/test/App.test.js
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import App from '../App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders website title', () => {
+  render(<App />);
+  const websiteTitle = screen.getByText(/When to Wash/i);
+  expect(websiteTitle).toBeInTheDocument();
+})
