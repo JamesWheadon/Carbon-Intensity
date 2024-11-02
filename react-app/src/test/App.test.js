18b8ed4bc8f7856e875ef7d9be6a293925885b72
JamesWheadon <james.wheadon@yahoo.com>
2024-11-02 01:59:24 +0000

    remove generated starter code

--- react-app/src/test/App.test.js
+++ react-app/src/test/App.test.js
@@ -2,18 +2,6 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import App from '../App';
 
-test('renders learn react link', () => {
-    render(<App />);
-
-    expect(screen.getByText(/learn react/i)).toBeInTheDocument();
-});
-
-test('renders website title', () => {
-    render(<App />);
-
-    expect(screen.getByText(/When to Wash/i)).toBeInTheDocument();
-});
-
 test('no best charge time displayed when no data', async () => {
     render(<App />);
 
