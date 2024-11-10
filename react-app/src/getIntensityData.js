94dfc499dcc0dc96f03318deb7d8ee1fd7fc8f48
JamesWheadon <james.wheadon@yahoo.com>
2024-11-10 18:52:32 +0000

    get intensity data in format for graph

--- react-app/src/getIntensityData.js
+++ react-app/src/getIntensityData.js
@@ -2,5 +2,16 @@ import axios from 'axios';
 
 export async function getIntensityData() {
     const response = await axios.post('http://localhost:9000/intensities');
-    return response.data;
+    return intensitiesToTimeData(response.data.intensities);
+}
+
+function intensitiesToTimeData(intensities) {
+    console.log(typeof intensities);
+    return intensities.map((dataPoint, index) => {
+        var h = Math.floor(index / 2);
+        var m = index % 2 == 0 ? '15' : '45';
+        h = (h < 10) ? '0' + h : h;
+        m = (m < 10) ? '0' + m : m;
+        return { time: h + ':' + m, intensity: dataPoint };
+    })
 }
--- react-app/src/test/getIntensityData.test.js
+++ react-app/src/test/getIntensityData.test.js
@@ -4,10 +4,10 @@ import { getIntensityData } from '../getIntensityData';
 jest.mock('axios');
 
 test('retrieves intensity data', async () => {
-	axios.post.mockImplementation(() => Promise.resolve({ data: { 'intensities': [212,150,175,212,150,175,212,150,175,212,150,175,212,150,175,212,150,175,212,150,175,212,150,175,212,150,175,212,150,175,212,150,175,212,150,175,212,150,175,212,150,175,212,150,175,212,150,175] } }));
+	axios.post.mockImplementation(() => Promise.resolve({ data: { 'intensities': [212,150,175] } }));
 
 	const result = await getIntensityData();
 
 	expect(axios.post).toHaveBeenCalledWith('http://localhost:9000/intensities');
-	expect(result).toStrictEqual({'intensities': [212,150,175,212,150,175,212,150,175,212,150,175,212,150,175,212,150,175,212,150,175,212,150,175,212,150,175,212,150,175,212,150,175,212,150,175,212,150,175,212,150,175,212,150,175,212,150,175]});
+	expect(result).toStrictEqual([{"time": "00:15", "intensity": 212},{"time": "00:45", "intensity": 150},{"time": "01:15", "intensity": 175}]);
 });
