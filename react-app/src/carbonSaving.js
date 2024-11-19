8de458dd39da5fa734cc9ca1648dff0b54febf80
JamesWheadon <james.wheadon@yahoo.com>
2024-11-19 18:54:08 +0000

    remove log lines

--- react-app/src/carbonSaving.js
+++ react-app/src/carbonSaving.js
@@ -7,12 +7,8 @@ export function dateTimeToDisplayTime(dateTime) {
 }
 
 export function getCarbonSaving(chargeTime, intensityData, duration, comparisonDate) {
-    console.log(intensityData[0].time)
-    console.log(chargeTime)
     const chargeMinutes = Math.floor((chargeTime - intensityData[0].time) / (15 * 60000));
     const currentMinutes = Math.floor((comparisonDate - intensityData[0].time) / (15 * 60000));
-    console.log(chargeMinutes);
-    console.log(currentMinutes);
     var chargeTotal = 0;
     var currentTotal = 0;
     for (var i = 0; i < duration / 15; i++) {
