0d81a72a4d84a6648d41afc65c17bef2f3f223d6
JamesWheadon <james.wheadon@yahoo.com>
2025-01-16 21:56:13 +0000

    check handling of invalid requests

--- WebApp/src/test/kotlin/com/intensity/OctopusContractTest.kt
+++ WebApp/src/test/kotlin/com/intensity/OctopusContractTest.kt
@@ -12,6 +12,7 @@ import org.http4k.core.HttpHandler
 import org.http4k.core.Method.GET
 import org.http4k.core.Request
 import org.http4k.core.Response
+import org.http4k.core.Status.Companion.BAD_REQUEST
 import org.http4k.core.Status.Companion.NOT_FOUND
 import org.http4k.core.Status.Companion.OK
 import org.http4k.core.Uri
@@ -25,6 +26,7 @@ import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import java.time.Instant
 import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeParseException
 import java.time.temporal.TemporalAccessor
 import kotlin.math.min
 
@@ -102,6 +104,37 @@ abstract class OctopusContractTest {
             equalTo(Failure("Incorrect Octopus tariff code"))
         )
     }
+
+    @Test
+    fun `handles invalid time periods`() {
+        assertThat(
+            octopus.prices(
+                "AGILE-FLEX-22-11-25",
+                "E-1R-AGILE-FLEX-22-11-25-C",
+                "2023-03-28T01:00Z",
+                "2023-03-27T04:59Z"
+            ),
+            equalTo(Failure("Invalid request"))
+        )
+        assertThat(
+            octopus.prices(
+                "AGILE-FLEX-22-11-25",
+                "E-1R-AGILE-FLEX-22-11-25-C",
+                "2023-03-28T01:00Z",
+                "2023-03-27T04:59Z"
+            ),
+            equalTo(Failure("Invalid request"))
+        )
+        assertThat(
+            octopus.prices(
+                "AGILE-FLEX-22-11-25",
+                "E-1R-AGILE-FLEX-22-11-25-C",
+                "2023-03-28T01:00Z",
+                "2023-03-27T0459Z"
+            ),
+            equalTo(Failure("Invalid request"))
+        )
+    }
 }
 
 interface Octopus {
@@ -123,6 +156,7 @@ class OctopusCloud(val httpHandler: HttpHandler) : Octopus {
         )
         return when (response.status) {
             OK -> Success(pricesLens(response))
+            BAD_REQUEST -> Failure("Invalid request")
             else -> octopusErrorLens(response).toFailure()
         }
     }
@@ -169,32 +203,45 @@ class FakeOctopus : HttpHandler {
             val productCode = request.path("productCode")!!
             val tariffCode = request.path("tariffCode")!!
             if (products.contains(productCode) && tariffCodePrices.keys.any { it.first == tariffCode }) {
-                val periodFrom =
+                val errors = mutableListOf<String>()
+                val periodFrom = try {
                     Instant.ofEpochSecond(
                         parseTimestamp(request.query("period_from")!!).epochSecond / 1800 * 1800
                     )
-                val periodTo =
+                } catch (e: DateTimeParseException) {
+                    errors.add(""""period_from":["Enter a valid date/time."]""")
+                    Instant.now()
+                }
+                val periodTo = try {
                     Instant.ofEpochSecond(
                         parseTimestamp(request.query("period_to")!!).epochSecond / 1800 * 1800 + 1800
                     )
-
-                val halfHourPrices = mutableListOf<String>()
-                val halfHourIntervals = ((periodTo.epochSecond - periodFrom.epochSecond) / 1800).toInt()
-                val providedPriceData = tariffCodePrices[tariffCode to periodFrom.toString()]!!.size
-                val count = min(halfHourIntervals, providedPriceData)
-                for (i in 0 until count) {
-                    halfHourPrices.add(
-                        """{
+                } catch (e: DateTimeParseException) {
+                    errors.add(""""period_to":["Enter a valid date/time."]""")
+                    Instant.now()
+                }
+                if (errors.isNotEmpty()) {
+                    Response(BAD_REQUEST).body(errors.joinToString(",", "{", "}"))
+                } else if (periodTo.isBefore(periodFrom)) {
+                    Response(BAD_REQUEST).body("""{"period_from":["Must not be greater than `period_to`."]}""")
+                } else {
+                    val halfHourPrices = mutableListOf<String>()
+                    val halfHourIntervals = ((periodTo.epochSecond - periodFrom.epochSecond) / 1800).toInt()
+                    val providedPriceData = tariffCodePrices[tariffCode to periodFrom.toString()]!!.size
+                    val count = min(halfHourIntervals, providedPriceData)
+                    for (i in 0 until count) {
+                        halfHourPrices.add(
+                            """{
                             "value_exc_vat":${tariffCodePrices[tariffCode to periodFrom.toString()]!![count - 1 - i]},
                             "value_inc_vat":${tariffCodePrices[tariffCode to periodFrom.toString()]!![count - 1 - i] * 1.05},
                             "valid_from":"${periodFrom.plusSeconds(i * 1800L)}",
                             "valid_to":"${periodFrom.plusSeconds((i + 1) * 1800L)}",
                             "payment_method":null
                         }"""
-                    )
-                }
-                Response(OK).body(
-                    """
+                        )
+                    }
+                    Response(OK).body(
+                        """
                     {
                         "count":${count},
                         "next":null,
@@ -202,7 +249,8 @@ class FakeOctopus : HttpHandler {
                         "results":
                         ${halfHourPrices.reversed().joinToString(",", "[", "]")}
                     }""".trimIndent()
-                )
+                    )
+                }
             } else if (products.contains(productCode)) {
                 Response(NOT_FOUND).body("""{"detail":"No ElectricityTariff matches the given query."}""")
             } else {
