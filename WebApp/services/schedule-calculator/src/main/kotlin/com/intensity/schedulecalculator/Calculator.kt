4507a8e9452759813bff80aae729fd484941065d
JamesWheadon <james.wheadon@yahoo.com>
2025-01-28 21:05:58 +0000

    normalize price and intensity data for electricity

--- WebApp/services/schedule-calculator/src/main/kotlin/com/intensity/schedulecalculator/Calculator.kt
+++ WebApp/services/schedule-calculator/src/main/kotlin/com/intensity/schedulecalculator/Calculator.kt
@@ -2,8 +2,28 @@ package com.intensity.schedulecalculator
 
 import java.math.BigDecimal
 import java.math.RoundingMode
+import java.time.ZonedDateTime
+
+fun normalize(electricity: Electricity): Electricity {
+    val normalizedPrices = normalize(electricity.slots.map { it.price })
+    val normalizedIntensities = normalize(electricity.slots.map { it.intensity })
+    return Electricity(electricity.slots.mapIndexed { index, slot ->
+        slot.copy(
+            price = normalizedPrices[index],
+            intensity = normalizedIntensities[index]
+        )
+    })
+}
 
 fun normalize(values: List<BigDecimal>): List<BigDecimal> {
     val max = values.max()
     return values.map { it.divide(max, 5, RoundingMode.HALF_UP) }
 }
+
+data class Electricity(val slots: List<HalfHourElectricity>)
+data class HalfHourElectricity(
+    val from: ZonedDateTime,
+    val to: ZonedDateTime,
+    val price: BigDecimal,
+    val intensity: BigDecimal
+)
--- WebApp/services/schedule-calculator/src/test/kotlin/com/intensity/schedulecalculator/CalculatorKtTest.kt
+++ WebApp/services/schedule-calculator/src/test/kotlin/com/intensity/schedulecalculator/CalculatorKtTest.kt
@@ -4,26 +4,54 @@ import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
 import org.junit.jupiter.api.Test
 import java.math.BigDecimal
+import java.time.ZonedDateTime
+
+typealias BD = BigDecimal
 
 class CalculatorKtTest {
     @Test
     fun `normalizes all values in a list so they are all a ratio of the largest`() {
-        val normalized = normalize(listOf(BigDecimal("2"), BigDecimal("4"), BigDecimal("3")))
+        val normalized = normalize(listOf(BD("2"), BD("4"), BD("3")))
 
-        assertThat(normalized, equalTo(listOf(BigDecimal("0.50000"), BigDecimal("1.00000"), BigDecimal("0.75000"))))
+        assertThat(normalized, equalTo(listOf(BD("0.50000"), BD("1.00000"), BD("0.75000"))))
     }
 
     @Test
     fun `normalizes handles negative values`() {
-        val normalized = normalize(listOf(BigDecimal("2"), BigDecimal("-4"), BigDecimal("1")))
+        val normalized = normalize(listOf(BD("2"), BD("-4"), BD("1")))
 
-        assertThat(normalized, equalTo(listOf(BigDecimal("1.00000"), BigDecimal("-2.00000"), BigDecimal("0.50000"))))
+        assertThat(normalized, equalTo(listOf(BD("1.00000"), BD("-2.00000"), BD("0.50000"))))
     }
 
     @Test
     fun `normalizes handles recursive values`() {
-        val normalized = normalize(listOf(BigDecimal("2"), BigDecimal("3"), BigDecimal("1")))
+        val normalized = normalize(listOf(BD("2"), BD("3"), BD("1")))
 
-        assertThat(normalized, equalTo(listOf(BigDecimal("0.66667"), BigDecimal("1.00000"), BigDecimal("0.33333"))))
+        assertThat(normalized, equalTo(listOf(BD("0.66667"), BD("1.00000"), BD("0.33333"))))
     }
+
+    @Test
+    fun `normalizes electricity data`() {
+        val electricity = Electricity(
+            listOf(
+                halfHourSlot(BD("10.14"), BD("53")),
+                halfHourSlot(BD("12.40"), BD("58")),
+                halfHourSlot(BD("11.67"), BD("63"))
+            )
+        )
+
+        val normalizedElectricity = normalize(electricity)
+
+        assertThat(
+            normalizedElectricity.slots.map { it.price },
+            equalTo(listOf(BD("0.81774"), BD("1.00000"), BD("0.94113")))
+        )
+        assertThat(
+            normalizedElectricity.slots.map { it.intensity },
+            equalTo(listOf(BD("0.84127"), BD("0.92063"), BD("1.00000")))
+        )
+    }
+
+    private fun halfHourSlot(price: BigDecimal, intensity: BigDecimal) =
+        HalfHourElectricity(ZonedDateTime.now(), ZonedDateTime.now(), price, intensity)
 }
