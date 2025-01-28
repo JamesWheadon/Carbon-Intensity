20da00492dc3105a53264faa4994bf715861b24d
JamesWheadon <james.wheadon@yahoo.com>
2025-01-28 18:02:57 +0000

    handle recurring numbers in big decimal division

--- WebApp/services/schedule-calculator/src/main/kotlin/com/intensity/schedulecalculator/Calculator.kt
+++ WebApp/services/schedule-calculator/src/main/kotlin/com/intensity/schedulecalculator/Calculator.kt
@@ -1,6 +1,7 @@
 package com.intensity.schedulecalculator
 
 import java.math.BigDecimal
+import java.math.RoundingMode
 
 fun calculate(intensities: List<Any>, prices: List<Any>, intensityWeight: Int, priceWeight: Int): List<Any> {
     return listOf()
@@ -8,5 +9,5 @@ fun calculate(intensities: List<Any>, prices: List<Any>, intensityWeight: Int, p
 
 fun normalize(values: List<BigDecimal>): List<BigDecimal> {
     val max = values.max()
-    return values.map { it.divide(max) }
+    return values.map { it.divide(max, 5, RoundingMode.HALF_UP) }
 }
--- WebApp/services/schedule-calculator/src/test/kotlin/com/intensity/schedulecalculator/CalculatorKtTest.kt
+++ WebApp/services/schedule-calculator/src/test/kotlin/com/intensity/schedulecalculator/CalculatorKtTest.kt
@@ -18,6 +18,20 @@ class CalculatorKtTest {
     fun `normalizes all values in a list so they are all a ratio of the largest`() {
         val normalized = normalize(listOf(BigDecimal("2"), BigDecimal("4"), BigDecimal("3")))
 
-        assertThat(normalized, equalTo(listOf(BigDecimal("0.5"), BigDecimal("1"), BigDecimal("0.75"))))
+        assertThat(normalized, equalTo(listOf(BigDecimal("0.50000"), BigDecimal("1.00000"), BigDecimal("0.75000"))))
+    }
+
+    @Test
+    fun `normalizes handles negative values`() {
+        val normalized = normalize(listOf(BigDecimal("2"), BigDecimal("-4"), BigDecimal("1")))
+
+        assertThat(normalized, equalTo(listOf(BigDecimal("1.00000"), BigDecimal("-2.00000"), BigDecimal("0.50000"))))
+    }
+
+    @Test
+    fun `normalizes handles recursive values`() {
+        val normalized = normalize(listOf(BigDecimal("2"), BigDecimal("3"), BigDecimal("1")))
+
+        assertThat(normalized, equalTo(listOf(BigDecimal("0.66667"), BigDecimal("1.00000"), BigDecimal("0.33333"))))
     }
 }
