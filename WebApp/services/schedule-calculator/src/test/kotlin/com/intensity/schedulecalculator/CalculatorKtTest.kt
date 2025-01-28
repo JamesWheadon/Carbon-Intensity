245dba6ece34066d59c19341caad55cd1f08f932
JamesWheadon <james.wheadon@yahoo.com>
2025-01-28 20:52:27 +0000

    remove calculate method

--- WebApp/services/schedule-calculator/src/test/kotlin/com/intensity/schedulecalculator/CalculatorKtTest.kt
+++ WebApp/services/schedule-calculator/src/test/kotlin/com/intensity/schedulecalculator/CalculatorKtTest.kt
@@ -2,18 +2,10 @@ package com.intensity.schedulecalculator
 
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
-import com.natpryce.hamkrest.isA
 import org.junit.jupiter.api.Test
 import java.math.BigDecimal
 
 class CalculatorKtTest {
-    @Test
-    fun `accepts the intensity and pricing data and returns time bands`() {
-        val schedule = calculate(listOf(), listOf(), 5, 5)
-
-        assertThat(schedule, isA<List<Any>>())
-    }
-
     @Test
     fun `normalizes all values in a list so they are all a ratio of the largest`() {
         val normalized = normalize(listOf(BigDecimal("2"), BigDecimal("4"), BigDecimal("3")))
