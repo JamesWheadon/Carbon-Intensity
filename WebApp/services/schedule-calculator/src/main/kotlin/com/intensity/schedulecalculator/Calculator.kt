06b49568b57ceff8dc9218aedf91264208e0f585
JamesWheadon <james.wheadon@yahoo.com>
2025-01-28 17:50:37 +0000

    normalize a list of big decimals so they are comparable

--- WebApp/services/schedule-calculator/src/main/kotlin/com/intensity/schedulecalculator/Calculator.kt
+++ WebApp/services/schedule-calculator/src/main/kotlin/com/intensity/schedulecalculator/Calculator.kt
@@ -1,5 +1,12 @@
 package com.intensity.schedulecalculator
 
+import java.math.BigDecimal
+
 fun calculate(intensities: List<Any>, prices: List<Any>, intensityWeight: Int, priceWeight: Int): List<Any> {
     return listOf()
+}
+
+fun normalize(values: List<BigDecimal>): List<BigDecimal> {
+    val max = values.max()
+    return values.map { it.divide(max) }
 }
--- WebApp/services/schedule-calculator/src/test/kotlin/com/intensity/schedulecalculator/CalculatorKtTest.kt
+++ WebApp/services/schedule-calculator/src/test/kotlin/com/intensity/schedulecalculator/CalculatorKtTest.kt
@@ -1,8 +1,10 @@
 package com.intensity.schedulecalculator
 
 import com.natpryce.hamkrest.assertion.assertThat
+import com.natpryce.hamkrest.equalTo
 import com.natpryce.hamkrest.isA
 import org.junit.jupiter.api.Test
+import java.math.BigDecimal
 
 class CalculatorKtTest {
     @Test
@@ -10,5 +12,12 @@ class CalculatorKtTest {
         val schedule = calculate(listOf(), listOf(), 5, 5)
 
         assertThat(schedule, isA<List<Any>>())
+    }
+
+    @Test
+    fun `normalizes all values in a list so they are all a ratio of the largest`() {
+        val normalized = normalize(listOf(BigDecimal("2"), BigDecimal("4"), BigDecimal("3")))
+
+        assertThat(normalized, equalTo(listOf(BigDecimal("0.5"), BigDecimal("1"), BigDecimal("0.75"))))
     }
 }
