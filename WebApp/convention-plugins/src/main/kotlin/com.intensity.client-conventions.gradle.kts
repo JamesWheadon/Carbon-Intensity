dd0686409fee1554bca8670787ab1945823a6479
JamesWheadon <james.wheadon@yahoo.com>
2025-03-03 22:45:44 +0000

    use testFixtures to separate production and test code

---
+++ WebApp/convention-plugins/src/main/kotlin/com.intensity.client-conventions.gradle.kts
@@ -0,0 +1,4 @@
+plugins {
+    id("com.intensity.kotlin-conventions")
+    id("java-test-fixtures")
+}
--- WebApp/services/central/build.gradle.kts
+++ WebApp/services/central/build.gradle.kts
@@ -14,6 +14,7 @@ dependencies {
     implementation(libs.result4k)
 
     testImplementation(projects.libraries.coreTest)
+    testImplementation(testFixtures(projects.clients.nationalGrid))
 
     testImplementation(libs.junit.engine)
     testImplementation(libs.http4k.hamkrest)
