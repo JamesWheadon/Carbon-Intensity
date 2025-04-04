2a72b46a50c72e130b15de22ada9c0f95df4ccba
JamesWheadon <james.wheadon@yahoo.com>
2025-04-04 16:11:30 +0100

    remove scheduler python app

--- scheduler/main.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from src.app import create_app
-from src.scheduler import UseTimeScheduler
-
-app = create_app(UseTimeScheduler())
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
