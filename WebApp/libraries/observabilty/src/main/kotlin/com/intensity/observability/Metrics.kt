2771c77a27860fb1ce479539b8d41c81cdd12559
JamesWheadon <james.wheadon@yahoo.com>
2025-07-18 01:01:00 +0100

    only create a counter once

--- WebApp/libraries/observabilty/src/main/kotlin/com/intensity/observability/Metrics.kt
+++ WebApp/libraries/observabilty/src/main/kotlin/com/intensity/observability/Metrics.kt
@@ -1,10 +1,15 @@
 package com.intensity.observability
 
 import io.opentelemetry.api.OpenTelemetry
+import io.opentelemetry.api.metrics.LongCounter
 
 class Metrics(private val openTelemetry: OpenTelemetry) {
+    private val registry = mutableMapOf<MetricName, LongCounter>()
     fun measure(metric: Metric) {
-        openTelemetry.getMeter("com.intensity.observability").counterBuilder(metric.name.value).build().add(1)
+        registry.getOrPut(metric.name) {
+            openTelemetry.getMeter("com.intensity.observability").counterBuilder(metric.name.value).build()
+        }
+            .add(1)
     }
 }
 
--- WebApp/libraries/observabilty/src/test/kotlin/com/intensity/observability/MetricsTest.kt
+++ WebApp/libraries/observabilty/src/test/kotlin/com/intensity/observability/MetricsTest.kt
@@ -2,6 +2,14 @@ package com.intensity.observability
 
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
+import io.opentelemetry.api.OpenTelemetry
+import io.opentelemetry.api.metrics.DoubleGaugeBuilder
+import io.opentelemetry.api.metrics.DoubleHistogramBuilder
+import io.opentelemetry.api.metrics.LongCounterBuilder
+import io.opentelemetry.api.metrics.LongUpDownCounterBuilder
+import io.opentelemetry.api.metrics.Meter
+import io.opentelemetry.api.trace.TracerProvider
+import io.opentelemetry.context.propagation.ContextPropagators
 import io.opentelemetry.sdk.OpenTelemetrySdk
 import io.opentelemetry.sdk.metrics.SdkMeterProvider
 import io.opentelemetry.sdk.testing.exporter.InMemoryMetricReader
@@ -18,4 +26,41 @@ class MetricsTest {
         val metricData = metricReader.collectAllMetrics().first { it.name == "testMetric" }
         assertThat(metricData.longSumData.points.sumOf { it.value }, equalTo(1))
     }
+
+    @Test
+    fun `only creates a counter once`() {
+        val spy = OpenTelemetrySpy(openTelemetry)
+        val metrics = Metrics(spy)
+        metrics.measure(Metric(MetricName("testMetric")))
+        metrics.measure(Metric(MetricName("testMetric")))
+
+        assertThat(spy.metricsCreated(), equalTo(listOf("testMetric")))
+        val metricData = metricReader.collectAllMetrics().first { it.name == "testMetric" }
+        assertThat(metricData.longSumData.points.sumOf { it.value }, equalTo(2))
+    }
+}
+
+class OpenTelemetrySpy(private val openTelemetry: OpenTelemetry) : OpenTelemetry {
+    private val meters = mutableListOf<MeterSpy>()
+    override fun getTracerProvider(): TracerProvider = openTelemetry.tracerProvider
+
+    override fun getPropagators(): ContextPropagators = openTelemetry.propagators
+
+    override fun getMeter(instrumentationScopeName: String): Meter {
+        return MeterSpy(openTelemetry.getMeter(instrumentationScopeName)).also { meters.add(it) }
+    }
+
+    fun metricsCreated() = meters.flatMap { it.countersMade }
+}
+
+class MeterSpy(private val meter: Meter) : Meter {
+    var countersMade = mutableListOf<String>()
+
+    override fun counterBuilder(p0: String): LongCounterBuilder = meter.counterBuilder(p0).also { countersMade.add(p0) }
+
+    override fun upDownCounterBuilder(p0: String): LongUpDownCounterBuilder = meter.upDownCounterBuilder(p0)
+
+    override fun histogramBuilder(p0: String): DoubleHistogramBuilder = meter.histogramBuilder(p0)
+
+    override fun gaugeBuilder(p0: String): DoubleGaugeBuilder = meter.gaugeBuilder(p0)
 }
